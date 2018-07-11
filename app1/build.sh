d43f8463a80a2e57ed7877190fe15aacf4b7412c
Ethan Daniel <ethan.w.daniel13@gmail.com>
2018-07-10 22:58:08 -0500

    not running containers interactively

--- app1/build.sh
+++ app1/build.sh
@@ -17,7 +17,7 @@ IMAGE_ID=$(docker images -q | head -n 1)
 echo "Image ID: $IMAGE_ID"
 
 echo "Running build container..."
-docker run -it --name $CONTAINER_NAME $IMAGE_ID
+docker run --name $CONTAINER_NAME $IMAGE_ID
 
 CONTAINER_ID=$(docker container ls -aq --filter name=$CONTAINER_NAME)
 echo "Container ID: $CONTAINER_ID"
